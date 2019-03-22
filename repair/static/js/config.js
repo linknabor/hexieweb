89b92b07a547885f65677e8d3f311206da13e86a
天使之翼唐 <476749176@qq.com>
2019-03-22 11:24:42 +0800

    兴社区测试

---
+++ repair/static/js/config.js
@@ -0,0 +1,10 @@
+let config = {
+	repair_orderdetail:{
+		url1:/127|test/.test(location.origin)?'https://test.e-shequ.com/pay/'+'shijiazhuang'+'orderpay.html':
+	    /uat/.test(location.origin)?'https://uat.e-shequ.com/weixin/'+ 'shijiazhuang'+'orderpay.html':
+	    'https://www.e-shequ.com/pay/'+'shijiazhuang'+'orderpay.html',	
+	}
+			
+}
+
+export default config
