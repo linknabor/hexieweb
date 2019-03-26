dc5cf3a5bad8198e57f823b787d8c2d3812cc530
天使之翼唐 <476749176@qq.com>
2019-03-26 16:02:27 +0800

    添加提取url参数的方法

--- wuye/invoice.js
+++ wuye/invoice.js
@@ -1,5 +1,9 @@
 $(document).ready(function() {
-	
+	function getUrlParam(name) {
+		var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)"); //构造一个含有目标参数的正则表达式对象
+		var r = window.location.search.substr(1).match(reg);  //匹配目标参数
+		if (r != null) return unescape(r[2]); return null; //返回参数值
+	}
 
 	var trade_water_id  =getUrlParam("trade_water_id");//交易编号,接口ur获取
 	var type = '01';//类型’
