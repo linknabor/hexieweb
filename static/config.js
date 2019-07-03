c6c4270a6a44d58298e20af409539c75db1e9877
davidhardson <davidhardson@hotmail.com>
2019-07-03 19:00:38 +0800

    公众号无账单绑定房屋成功后无提示

---
+++ static/config.js
@@ -0,0 +1,33 @@
+var pay = /127|test/.test(location.origin)?'https://test.e-shequ.com/pay/':
+		  /uat/.test(location.origin)?'https://uat.e-shequ.com/pay/':
+		  'https://www.e-shequ.com/pay/';
+
+var html= /127|test/.test(location.origin)?'https://test.e-shequ.com/weixin/':
+		  /uat/.test(location.origin)?'https://uat.e-shequ.com/weixin/':
+          'https://www.e-shequ.com/weixin/';
+          
+var url = /127|test/.test(location.origin)?'https://test.e-shequ.com/wechat/hexie/wechat/':
+          /uat/.test(location.origin)?'https://uat.e-shequ.com/wechat/hexie/wechat/':
+          'https://www.e-shequ.com/wechat/hexie/wechat/';
+
+window.config={
+    //公共的请求地址
+    URL:url,
+    //公共的底部跳转
+    footer:{
+        wuye:html+'/wuye/index.html?v=20162299',
+        person:html+'/person/index.html?v=20160229',
+    },
+    wuye_myhouse:{
+        url: /127|test/.test(location.origin)?'test.e-shequ.com':
+                /uat/.test(location.origin)?'uat.e-shequ.com':
+                'www.e-shequ.com'       //提示框网址
+    },
+    // 物业支付
+    wuye_pay:{
+        url:html+'paymentdetail.html?#/?'
+    }
+   
+}
+
+export default config
