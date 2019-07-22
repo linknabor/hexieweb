9047a452957015d8c6de852e54d02d409509f4bc
zhaoshiwei <861812342.@qq.com>
2019-07-22 17:03:26 +0800

    新支付3

--- pay/buy.js
+++ pay/buy.js
@@ -89,7 +89,16 @@ avalon.ready(function() {
         a = "/requestPay/"+o.model.order.id,
         i = null,
         e = function(n) {
+			wx.config({
+				debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+				appId: n.result.appId, // 必填，公众号的唯一标识
+				timestamp: n.result.timestamp, // 必填，生成签名的时间戳
+				nonceStr: n.result.nonceStr, // 必填，生成签名的随机串
+				signature: n.result.signature,// 必填，签名，见附录1
+				jsApiList: ['chooseWXPay'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+			});
         	wx.chooseWXPay({
+			  "appId":n.result.appId,
               "timestamp":n.result.timestamp,
               "nonceStr":n.result.nonceStr,
               "package":n.result.pkgStr,
